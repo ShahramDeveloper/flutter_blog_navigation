5717e788331ffc25df52bac62bb53375a32237e7
Shahram <shahramdeveloper11@gmail.com>
2024-10-11 12:00:31 +0330

    Remove redundant comment from pubspec.yaml And add SecondScreen class to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,27 @@ class HomeScreen extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-            child: Center(
-          child: TextButton(onPressed: () {}, child: const Text('Next')),
-        )),
+          child: Center(
+            child: TextButton(onPressed: () {}, child: const Text('Next')),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: Text('Second screen'),
+          ),
+        ),
       ),
     );
   }
