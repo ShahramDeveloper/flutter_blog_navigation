e39735a8493a86aea96e95e6c7417c28337820bf
Shahram <shahramdeveloper11@gmail.com>
2024-10-14 20:29:34 +0330

    Change LoginPage in login_page.dart ui.

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -16,70 +16,60 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.deepOrangeAccent,
+      backgroundColor: Colors.white,
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'خوش آمدید',
-                    style:
-                        TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
-                  ),
-                  SizedBox(
-                    width: 8,
-                  ),
-                  Icon(
-                    Icons.login,
-                    size: 24.0,
-                  )
-                ],
-              ),
-              Image(
-                image: AssetImage('images/welcome.png'),
-              ),
-              OutlinedButton(
-                onPressed: () {
-                  navigateToAScreen(context, BlogScreen());
-                },
-                child: Text(
-                  'ورود به حساب',
-                  style: TextStyle(fontSize: 16.0),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 20,
                 ),
-                style: OutlinedButton.styleFrom(
-                  minimumSize: Size(200.0, 40.0),
-                  foregroundColor: Colors.white,
-                  side: BorderSide(
-                    color: Colors.white,
-                    width: 2.0,
+                Text(
+                  ' خوش آمدید VIP به اپلیکیشن سیگنال ',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+                ),
+                Image(
+                  image: AssetImage('images/w.png'),
+                ),
+                OutlinedButton(
+                  onPressed: () {
+                    navigateToAScreen(context, BlogScreen());
+                  },
+                  child: Text(
+                    'ورود به حساب',
+                    style: TextStyle(fontSize: 16.0),
+                  ),
+                  style: OutlinedButton.styleFrom(
+                    minimumSize: Size(200.0, 40.0),
+                    foregroundColor: Colors.black,
+                    side: BorderSide(
+                      color: Colors.black,
+                      width: 2.0,
+                    ),
                   ),
                 ),
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                child: Text('ثبت نام'),
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.black,
-                  minimumSize: Size(200.0, 40.0),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text('ثبت نام'),
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    backgroundColor: Colors.black,
+                    minimumSize: Size(200.0, 40.0),
+                  ),
                 ),
-              ),
-              TextButton(
-                onPressed: () {
-                  navigateToAScreen(context, PasswordRecovery());
-                },
-                child: Text(
-                  'فراموشی رمز عبور',
-                ),
-                style: TextButton.styleFrom(foregroundColor: Colors.white),
-              )
-            ],
+                TextButton(
+                  onPressed: () {
+                    navigateToAScreen(context, PasswordRecovery());
+                  },
+                  child: Text(
+                    'فراموشی رمز عبور',
+                  ),
+                  style: TextButton.styleFrom(foregroundColor: Colors.grey),
+                )
+              ],
+            ),
           ),
         ),
       ),
