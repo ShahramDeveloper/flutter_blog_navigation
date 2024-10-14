067f46349a837f02d2de77b0a135e0768cf1547e
Shahram <shahramdeveloper11@gmail.com>
2024-10-14 19:35:10 +0330

    Add Column to show items in BlogScreen and set one item to column.

--- lib/blog_page.dart
+++ lib/blog_page.dart
@@ -17,12 +17,65 @@ class BlogScreen extends StatelessWidget {
       body: SafeArea(
         child: Center(
           child: Padding(
-            padding: EdgeInsets.all(10),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(12),
-              child: Image(
-                image: AssetImage('images/s.png'),
-              ),
+            padding: EdgeInsets.all(8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: Image(
+                    image: AssetImage('images/s.png'),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  textAlign: TextAlign.center,
+                  ' برای ۱۴ آبان safeMoon سیگنال خرید ',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'فروش روی ۱۲،۵۶۰',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.red),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Icon(
+                      Icons.sell,
+                      size: 20,
+                      color: Colors.red,
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(
+                      'خرید روی ۱۲،۳۶۵',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.green),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Icon(
+                      Icons.price_check,
+                      size: 20,
+                      color: Colors.green,
+                    ),
+                  ],
+                )
+              ],
             ),
           ),
         ),
