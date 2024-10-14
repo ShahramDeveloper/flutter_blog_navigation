e18763ed5a54f50bf32c01fcf9506a2122a653a8
Shahram <shahramdeveloper11@gmail.com>
2024-10-14 11:44:02 +0330

    Change HomeScreen widget name to LoginPage. Create login_page.dart and cut LoginPage widget from main.dart to login_page.dart.

---
+++ lib/login_page.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepOrangeAccent,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'خوش آمدید',
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  Icon(
+                    Icons.login,
+                    size: 24.0,
+                  )
+                ],
+              ),
+              Image(
+                image: AssetImage('images/welcome.png'),
+              ),
+              OutlinedButton(
+                onPressed: () {},
+                child: Text(
+                  'ورود به حساب',
+                  style: TextStyle(fontSize: 16.0),
+                ),
+                style: OutlinedButton.styleFrom(
+                  minimumSize: Size(200.0, 40.0),
+                  foregroundColor: Colors.white,
+                  side: BorderSide(
+                    color: Colors.white,
+                    width: 2.0,
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('ثبت نام'),
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.black,
+                  minimumSize: Size(200.0, 40.0),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'login_page.dart';
+
 void main() {
   runApp(const Application());
 }
@@ -12,73 +14,7 @@ class Application extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: 'vazir'),
-      home: HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.deepOrangeAccent,
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'خوش آمدید',
-                    style:
-                        TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
-                  ),
-                  SizedBox(
-                    width: 8,
-                  ),
-                  Icon(
-                    Icons.login,
-                    size: 24.0,
-                  )
-                ],
-              ),
-              Image(
-                image: AssetImage('images/welcome.png'),
-              ),
-              OutlinedButton(
-                onPressed: () {},
-                child: Text(
-                  'ورود به حساب',
-                  style: TextStyle(fontSize: 16.0),
-                ),
-                style: OutlinedButton.styleFrom(
-                  minimumSize: Size(200.0, 40.0),
-                  foregroundColor: Colors.white,
-                  side: BorderSide(
-                    color: Colors.white,
-                    width: 2.0,
-                  ),
-                ),
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                child: Text('ثبت نام'),
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.black,
-                  minimumSize: Size(200.0, 40.0),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+      home: LoginPage(),
     );
   }
 }
